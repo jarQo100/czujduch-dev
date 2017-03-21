20e9fb4934a588f11a7c38fc258bcf823199bcb8
Jarosław Krupa <jarko_krupa@o2.pl>
2017-03-21 22:20:47 +0100

    Adding component Auth and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routing } from "./app.routing";
 import { AppComponent } from './app.component';
+import { SignInComponent } from './auth/sign-in.component';
+import { SignUpComponent } from './auth/sign-up.component';
+import { AuthComponent } from './auth/auth.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './content/navbar/navbar.component';
+import { MenuComponent } from './content/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    AuthComponent,
+    ContentComponent,
+    NavbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/sign-in.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./auth.component.css']
+})
+
+export class SignInComponent {
+
+}
